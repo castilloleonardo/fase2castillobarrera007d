9775fda218e4c64a806ba91f5a0615c66c5266af
castilloleonardo <le.castillom@alumnos.duoc.cl>
2020-11-04 01:52:31 -0300

    Creando Pagina de Inicio

--- musicoschile/index/urls.py
+++ musicoschile/index/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 
 urlpatterns = [
+    path('',views.index2, name='index2'),
 ]
--- musicoschile/index/views.py
+++ musicoschile/index/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from . models import Musico, Mensaje
+from django.views import generic
 
 # Create your views here.
